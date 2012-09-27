b87b894a477e0c8386ed4dd9ab8ee99599214495
OpenWRT <openwrt@openwrt.org>
2012-09-27 13:58:19 +0200

    generic/302-mips_use_generic_thread_info_allocator

--- arch/mips/include/asm/thread_info.h
+++ arch/mips/include/asm/thread_info.h
@@ -87,6 +87,7 @@ register struct thread_info *__current_thread_info __asm__("$28");
 
 #define STACK_WARN	(THREAD_SIZE / 8)
 
+#if 0
 #define __HAVE_ARCH_THREAD_INFO_ALLOCATOR
 
 #ifdef CONFIG_DEBUG_STACK_USAGE
@@ -98,6 +99,7 @@ register struct thread_info *__current_thread_info __asm__("$28");
 #endif
 
 #define free_thread_info(info) kfree(info)
+#endif
 
 #define PREEMPT_ACTIVE		0x10000000
 
